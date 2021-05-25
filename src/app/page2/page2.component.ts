f5feb1eee158399224eac24100cfe6ce4a93a0a5
Yoni12ab <yoni12ab@gmail.com>
2021-05-25 13:00:06 +0300

    added inner pages

--- src/app/page2/page2.component.ts
+++ src/app/page2/page2.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { SomeService } from '../some.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-page2',
